de6841c584f17a781a969f670ecc6505cac82564
Sena <sena250817@gmail.com>
2026-09-09 13:26:19 +0300

    fix: add missing InputImage, OutputImage and OutputDetections class definitions

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,7 +1,58 @@
 from sdks.novavision.src.base.model import Package, Configs, Inputs, Outputs, Request, Response, Config
 from pydantic import Field
 from typing import Union, Literal, Optional
-from sdks.novavision.src.base.model import InputImage, OutputImage, OutputDetections
+
+
+
+from pydantic import validator
+from typing import List
+from sdks.novavision.src.base.model import Image, Detection, Output, Input
+
+class InputImage(Input):
+    name: Literal["inputImage"] = "inputImage"
+    value: Union[List[Image], Image]
+    type: str = "object"
+
+    @validator("type", pre=True, always=True)
+    def set_type_based_on_value(cls, value, values):
+        value = values.get('value')
+        if isinstance(value, Image):
+            return "object"
+        elif isinstance(value, list):
+            return "list"
+        return "object"
+
+    class Config:
+        title = "Image"
+
+
+class OutputImage(Output):
+    name: Literal["outputImage"] = "outputImage"
+    value: Union[List[Image],Image]
+    type: str = "object"
+
+    @validator("type", pre=True, always=True)
+    def set_type_based_on_value(cls, value, values):
+        value = values.get('value')
+        if isinstance(value, Image):
+            return "object"
+        elif isinstance(value, list):
+            return "list"
+        return "object"
+
+    class Config:
+        title = "Image"
+
+
+class OutputDetections(Output):
+    name: Literal["outputDetections"] = "outputDetections"
+    value: List[Detection]
+    type: Literal["list"] = "list"
+
+    class Config:
+        title = "Detections"
+
+
 
 class Threshold(Config):
     name: Literal["threshold"] = "threshold"
